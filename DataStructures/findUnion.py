f1964e0ea95cb910589693bfc0863c7861f03958
sahilsnaik <sn220222@gmail.com>
2023-08-24 20:06:30 +0530

    dsa practise

---
+++ DataStructures/findUnion.py
@@ -0,0 +1,25 @@
+
+def find_union(arr1, arr2):
+    s = set()
+    union = []
+    
+    for num in arr1:
+        s.add(num)
+    
+    for num in arr2:
+        s.add(num)
+    
+    for num in s:
+        union.append(num)
+    
+    return union
+
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr2 = [2, 3, 4, 4, 5, 11, 12]
+
+union = find_union(arr1, arr2)
+
+print("Union of arr1 and arr2 is:")
+print(*union)
+
+
